f409c019ebbaf64a252f59de49af96916965d7b3
Пётр Юдин <udin.petr@gmail.com>
2021-01-07 19:25:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,27 +1,72 @@
-$(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger, .header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
-		$('.header').toggleClass('lock');
-    });
-
-    $('#form').submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$('.js-overlay-thank-you').fadeIn();
-			$(this).find('input').val('');
-			$('#form').trigger('reset');
-		});
-		return false;
-	});
-}); 
-
 $(function () {
     $("#phone").mask("8(999) 999-99-99");
 });
 
 
 
+document.addEventListener("DOMContentLoaded", () => {
+	//Burger Menu
+	let headerLock = document.querySelector(".header");
+	let burgerMenu = document.querySelector(".header__burger");
+	let headMenu = document.querySelector(".header__menu");
+	let body = document.querySelector("body");
+
+	burgerMenu.addEventListener("click", () => {
+		burgerMenu.classList.toggle('active');
+		headMenu.classList.toggle('active');
+		headerLock.classList.toggle('lock');
+		body.classList.toggle('lock');
+	});
+
+	//Button Top
+	let topBtn = document.querySelector('.back__top');
+
+	window.addEventListener('scroll', trackScroll);
+	topBtn.addEventListener('click', backToTop);	
+	
+	function trackScroll() {
+		let scrolled = window.pageYOffset;
+		let coords = document.documentElement.clientHeight;
+	
+		if (scrolled > coords) {
+		  topBtn.classList.add('back__top-show');
+		}
+		if (scrolled < coords) {
+		  topBtn.classList.remove('back__top-show');
+		}
+	  };
+
+	function backToTop() {
+		 if (window.pageYOffset > 0) {
+		   window.scrollBy(0, -80);
+		   setTimeout(backToTop, 0);
+		 }
+	  }
+
+
+	// form
+
+	const ajaxSend = async (formData) => {
+        const fetchResp = await fetch('mail.php', {
+            method: 'POST',
+            body: formData
+        });
+        if (!fetchResp.ok) {
+            alert('Ошибка при отпрвке');
+        }
+        return await fetchResp.text();
+    };
+
+    const form = document.getElementById("form");
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            const formData = new FormData(form);
+
+            ajaxSend(formData)
+                .then((response) => {
+                    console.log(response);
+                    form.reset();
+                })
+                .catch((err) => console.error(err))
+        });
+})
